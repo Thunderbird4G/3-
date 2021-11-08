b2ffdbf6fd44f4fd1bc9e791be7dc7738ae13289
Thunderbird4G <90564792+Thunderbird4G@users.noreply.github.com>
2021-11-08 06:06:26 +0300

    Add files via upload

---
+++ lab3-main/lab3_1/while/Header.h
@@ -0,0 +1,11 @@
+#include <math.h>
+double sum(int n);
+double summ2(double eps);
+void print(int n, int k);
+int findFirstElement(double eps);
+int findFirstNegativeElement(double eps);
+double f(int);
+
+double f(int i) {
+	return pow(-1, i) * (i + 1) / (pow(i, 3) - pow(i, 2) + 1);
+}
